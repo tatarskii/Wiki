4fd3bdcc4e22c05a8a894028fcba19bdda8135e6
tatarskii <mik.tatarskii@gmail.com>
2023-11-03 15:21:12 +0400

    fix: structure

--- main.py
+++ main.py
@@ -1,46 +1,8 @@
-import requests
-from bs4 import BeautifulSoup
-import re
+from parser import *
 
+if __name__ == "__main__":
+    tmp = Parser()
+    print(tmp.pars())
 
-class Parser:
-
-    def __init__(self):
-        pass
-
-    def zero_page(self):
-
-        wiki_url = input("Paste your link here: ")
-        try:
-            re.match("(https?://en\\.wikipedia\\.org/wiki/([a-zA-Z]|[0-9]))", wiki_url)
-            response = requests.get(wiki_url)
-            if response:
-                return response.text
-        except Exception as e:
-            print(e)
-
-    def pars(self):
-        bs = BeautifulSoup(self.zero_page(), 'html.parser')
-        link_list = []
-        try:
-            for link in bs.find_all('a'):
-                url = link.get("href")
-                if url not in link_list:
-                    link_list.append(url)
-            return link_list
-        except Exception as e:
-            print(e)
-
-    def bd_connect(self):
-        pass
-
-    def bd_create(self):
-        pass
-
-    def bd_write(self):
-        pass
-
-
-#tmp = Parser()
-#for i in tmp.pars():
-    #print(i)
+tmp = Parser()
+print(tmp.pars())
--- url_checker.py
+++ url_checker.py
@@ -2,20 +2,24 @@ import requests
 import re
 
 
+def start_page_check():
+
+    wiki_url = input("Paste your link here: ")
+    try:
+        re.match("(https?://en\\.wikipedia\\.org/wiki/([a-zA-Z]|[0-9]))", wiki_url)
+        return str(wiki_url)
+    except Exception as e:
+        print(e)
+
+
+def html_get():
+    response = requests.get(start_page_check())
+    if response:
+        return response.text
+
+
 class UrlCheck:
-    def start_page_check(self):
-
-        wiki_url = input("Paste your link here: ")
-        try:
-            re.match("(https?://en\\.wikipedia\\.org/wiki/([a-zA-Z]|[0-9]))", wiki_url)
-            return str(wiki_url)
-        except Exception as e:
-            print(e)
-
-    def html_get(self):
-        response = requests.get(self.start_page_check())
-        if response:
-            return response.text
+    pass
 
 
 tmp = UrlCheck()
