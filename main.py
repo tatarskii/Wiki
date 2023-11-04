8377384fc92c8d91c7d62dc9a355eb4e1e469874
tatarskii <mik.tatarskii@gmail.com>
2023-11-04 19:24:59 +0400

    minor commit

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from test import *
 #from parser import *
 
 tmp = GetPageLinks()
-print(tmp.find_ref_links_in_text())
-#print(tmp.get_links_dict())
+print(tmp.find_links_in_div_with_id())
+print(tmp.find_ref_links_in_col())
