6a5ff32169a48c3dc251feb66996bb30a2d81f3c
tatarskii <mik.tatarskii@gmail.com>
2023-11-06 03:37:01 +0400

    Still working

---
+++ data_base.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+
+def connect():
+    connection = sqlite3.connect('test.db')
+    cursor = connection.cursor()
+
+    # Create table if not exist
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Wiki_links (
+    id INTEGER PRIMARY KEY,
+    parent_link TEXT NOT NULL,
+    link TEXT NOT NULL,
+    title TEXT NOT NULL
+    )
+    ''')
+
+    connection.commit()
+    connection.close()
+
+
+def into():
+    connection = sqlite3.connect('test.db')
+    cursor = connection.cursor()
+
+    cursor.execute('INSERT INTO Wiki_links (parent_link, link, title) VALUES (?, ?, ?)',
+                   ('https://en.wikipedia.org/wiki/Dildo', 'https://en.wikipedia.org/wiki/Dildo', 'Dildo'))
+
+    connection.commit()
+    connection.close()
+
+
+def read():
+    connection = sqlite3.connect('test.db')
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Wiki_links')
+    parent_link = cursor.fetchall()
+
+    for parent_link in parent_link:
+        print(parent_link)
+
+    connection.close()
+
+
+class DataBase:
+    pass
+
+
+print(read())
